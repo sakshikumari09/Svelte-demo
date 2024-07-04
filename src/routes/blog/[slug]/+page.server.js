8622b6c0fb49e7eb71e80d53440ca9e4030e9cd1
Sakshi Kumari <sakshikumari992003@gmail.com>
2024-07-05 02:30:13 +0530

    initial commit

---
+++ src/routes/blog/[slug]/+page.server.js
@@ -0,0 +1,6 @@
+export async function load({params}){
+
+    return{
+        content : `Hello ${params.slug}`
+    }
+}
---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    export let data
+</script>
+
+<p class="font-bold text-center"> {data.content} </p>
